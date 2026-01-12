a2d0caf0a97dfbc6d38f27f6264a9bb3ee1f99e3
ahmedwaelalkaliby <ahmedalkaliby@gmail.com>
2026-01-12 17:39:25 +0200

    create folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-
-
 import './App.css'
-
 function App() {
-
-
   return (
     <>
       <h1 className="text-red-500 text-3xl font-bold">
---
+++ src/pages/IssueDetails.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function IssueDetails() {
+  return (
+    <div>IssueDetails</div>
+  )
+}
